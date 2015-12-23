2ba7728364740dc4e8335534d3f1cdd0c0f1a6e8
Peter Wallhead <peter.wallhead@gmail.com>
2015-12-24 07:58:50 +1100

    Improves responsive design and takes padding+margins into account during positioning

--- simple-card-layouter.js
+++ simple-card-layouter.js
@@ -5,12 +5,10 @@ function generateLayout() {
     var scp_left = 0;
     var scp_top = 0;
 
-    var scp_wrapper_width = $("#cards").width();
+    var scp_wrapper_width = $("#cards").outerWidth(true);
 
-    if(scp_wrapper_width < 520) {
+    if(scp_wrapper_width < 600) {
         var scp_cols = 1;
-    } else if(scp_wrapper_width > 520 && scp_wrapper_width < 768) {
-        var scp_cols = 2;
     } else {
         var scp_cols = 3;
     }
@@ -27,17 +25,17 @@ function generateLayout() {
         $(this).css({ top: scp_top });
         if(scp_r !== scp_cols) {
             scp_r++;
-            scp_left = scp_left+$(this).width();
+            scp_left = scp_left+$(this).outerWidth(true);
         } else {
             scp_r = 1;
             scp_left = 0;
             var scp_card_above = scp_i - scp_cols;
-            scp_top = $("#"+scp_idArray[scp_card_above-1]).height() + $("#"+scp_idArray[scp_card_above-1]).position().top;
+            scp_top = $("#"+scp_idArray[scp_card_above-1]).outerHeight(true) + $("#"+scp_idArray[scp_card_above-1]).position().top;
         }
 
         if(scp_top !== 0) {
             var scp_card_above = scp_i - scp_cols;
-            scp_top = $("#"+scp_idArray[scp_card_above-1]).height() + $("#"+scp_idArray[scp_card_above-1]).position().top;
+            scp_top = $("#"+scp_idArray[scp_card_above-1]).outerHeight(true) + $("#"+scp_idArray[scp_card_above-1]).position().top;
         }
     });
 }
